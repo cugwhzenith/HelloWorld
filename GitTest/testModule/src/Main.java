fe9f493f6c5e1ee222a2bf92fdf416476ba4bdcb
cugwhzenith <46958942+cugwhzenith@users.noreply.github.com>
2019-09-05 18:07:11 +0800

    Update Main.java

--- GitTest/testModule/src/Main.java
+++ GitTest/testModule/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("fist edited!");
         System.out.println("second edited!");
+        System.out.println("edited in Github!");
+
     }
 }
